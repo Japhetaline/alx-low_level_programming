97a4c8e6a71dc81792007b068534fc9df54205ad
Japhetaline <japhetdamassoh@gmail.com>
2023-02-17 10:07:02 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: Awlays 0 before successful
+ *
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}
