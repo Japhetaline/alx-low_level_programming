deb7a5035a871a96cf8fd693a5cb2533d66a29d4
Japhetaline <japhetdamassoh@gmail.com>
2023-02-17 11:18:14 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main -main function
+ *
+ * Return: Always 0 before success
+ *
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		printf("%d", i);
+	printf("\n");
+	return (0);
+
+}
