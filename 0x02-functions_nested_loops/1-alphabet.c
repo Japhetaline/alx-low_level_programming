b1e7148c4370223441086b5c2ad22ede52780265
Japhetaline <Japhetdamassoh@gmail.com>
2023-02-21 03:32:09 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that  print alphabeth in lower case
+ *
+ * Return: To be 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
