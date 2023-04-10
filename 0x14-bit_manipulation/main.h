2c9974cb520dad55eefa8f96ce12ff8f5b86b750
Japhetaline <Japhetdamassoh@gmail.com>
2023-04-10 11:13:48 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,9 +1,9 @@
-#ifndef _MAIN_H
-#define _MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 #include <stdlib.h>
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
-#endif 
+#endif /*_MAIN_H*/
