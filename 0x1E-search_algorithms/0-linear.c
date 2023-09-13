de743bf06a5ac1e5bc84185db6c556b34dad1746
Japhetaline <Japhetdamassoh@gmail.com>
2023-09-13 11:29:37 -0700

    search_algorithm

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,14 +11,15 @@
 
 int linear_search(int *array, size_t size, int value)
 {
-	size_t k = 0;
+	size_t i = 0;
 
 	if (!array || size <= 0)
 		return (-1);
-	for (k = 0; k < size; k++)
+	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%lu] = [%d]\n", k, array[k]);
-		if (array[k] == value)
-			return (k);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
 	}
 	return (-1);
+}
